ceb4ad6dd1e42b61e3eda6db08a5279968fe3800
Ahmed Mahdy <mo.a9tay@gmail.com>
2022-03-09 11:51:33 +0200

    fixing some errors try 4

--- abcd/models/cnn.py
+++ abcd/models/cnn.py
@@ -5,9 +5,11 @@ from tensorflow.keras.utils import to_categorical
 
 class CNN:
     @staticmethod
-    def model(classes, **args):
+    def model(height, width, depth, classes, **args):
+
+        inputShape = (height, width, depth)
         model = tf.keras.models.Sequential([
-                tf.keras.layers.Conv2D(256, (3,3), activation='relu', input_shape=(65, 41, 1), padding='same'),
+                tf.keras.layers.Conv2D(256, (3,3), activation='relu', input_shape=inputShape, padding='same'),
                 tf.keras.layers.MaxPooling2D((2,2)),
                 tf.keras.layers.Conv2D(64, (3,3), activation='relu', padding='same'),
                 tf.keras.layers.MaxPooling2D((2,2)),
